898e7ae76b479d9f1123e54768e9dee0e6f888aa
Arlo Belshee <arlo.belshee@live.com>
2014-09-09 12:52:16 -0700

    Let's see if we can do the git deploy using Grunt. This will probably fail. Just trying to switch between branches for now - which will clear working directories, including the Gruntfile & node_modules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,123 +2,139 @@
  * Scale-Out-Agile
  * 
  * Arlo Belshee
- * https://github.com/arlobelshee/Scale-Out-Agile
+ * https://github.com/Scale-Out-Agile/Scale-Out-Agile.github.io
  * 
  * Copyright (c) 2014
  * Licensed under the MIT license.
  */
 
-module.exports = function(grunt) {
-'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
+    // Project configuration.
+    grunt.initConfig({
 
-    // Project metadata
-    pkg   : grunt.file.readJSON('package.json'),
-    vendor: grunt.file.readJSON('.bowerrc').directory,
-    site  : grunt.file.readYAML('_config.yml'),
-    bootstrap: '<%= vendor %>/bootstrap',
-
-
-    // Before generating any new files, remove files from previous build.
-    clean: {
-      site: ['<%= site.dest %>/**/*.*', '<%= site.dest %>/*/*/', '<%= site.dest %>/*/']
-    },
+        // Project metadata
+        pkg: grunt.file.readJSON('package.json'),
+        vendor: grunt.file.readJSON('.bowerrc').directory,
+        site: grunt.file.readYAML('_config.yml'),
+        bootstrap: '<%= vendor %>/bootstrap',
 
 
-    // Lint JavaScript
-    jshint: {
-      all: ['Gruntfile.js', 'templates/helpers/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+        // Before generating any new files, remove files from previous build.
+        clean: {
+            site: ['<%= site.dest %>/**/*.*', '<%= site.dest %>/*/*/', '<%= site.dest %>/*/']
+        },
 
 
-    // Build HTML from templates and data
-    assemble: {
-      options: {
-        flatten: true,
-        production: true,
-        assets: '<%= site.assets %>',
-        postprocess: require('pretty'),
-
-        // Metadata
-        pkg: '<%= pkg %>',
-        site: '<%= site %>',
-        data: ['<%= site.data %>'],
-
-        // Templates
-        partials: '<%= site.includes %>',
-        layoutdir: '<%= site.layouts %>',
-        layout: '<%= site.layout %>',
-
-        // Extensions
-        helpers: '<%= site.helpers %>',
-        plugins: '<%= site.plugins %>'
-      },
-      site: {
-        files: {'<%= site.dest %>/': ['<%= site.templates %>/*.hbs']}
-      }
-    },
+        // Lint JavaScript
+        jshint: {
+            all: ['Gruntfile.js', 'templates/helpers/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
 
 
-    // Compile LESS to CSS
-    less: {
-      options: {
-        vendor: 'vendor',
-        paths: [
+        // Build HTML from templates and data
+        assemble: {
+            options: {
+                flatten: true,
+                production: true,
+                assets: '<%= site.assets %>',
+                postprocess: require('pretty'),
+
+                // Metadata
+                pkg: '<%= pkg %>',
+                site: '<%= site %>',
+                data: ['<%= site.data %>'],
+
+                // Templates
+                partials: '<%= site.includes %>',
+                layoutdir: '<%= site.layouts %>',
+                layout: '<%= site.layout %>',
+
+                // Extensions
+                helpers: '<%= site.helpers %>',
+                plugins: '<%= site.plugins %>'
+            },
+            site: {
+                files: { '<%= site.dest %>/': ['<%= site.templates %>/*.hbs'] }
+            }
+        },
+
+
+        // Compile LESS to CSS
+        less: {
+            options: {
+                vendor: 'vendor',
+                paths: [
           '<%= site.theme %>',
           '<%= site.theme %>/bootstrap',
           '<%= site.theme %>/components',
           '<%= site.theme %>/utils'
         ]
-      },
-      site: {
-        src: ['<%= site.theme %>/site.less'],
-        dest: '<%= site.assets %>/css/site.css'
-      }
-    },
+            },
+            site: {
+                src: ['<%= site.theme %>/site.less'],
+                dest: '<%= site.assets %>/css/site.css'
+            }
+        },
 
 
-    // Copy Bootstrap's assets to site assets
-    copy: {
-      assets: {
-        files: [
-          {expand: true, cwd: '<%= bootstrap %>/dist/fonts', src: ['*.*'], dest: '<%= site.assets %>/fonts/'},
-          {expand: true, cwd: '<%= bootstrap %>/dist/js',    src: ['*.*'], dest: '<%= site.assets %>/js/'},
+        // Copy Bootstrap's assets to site assets
+        copy: {
+            assets: {
+                files: [
+          { expand: true, cwd: '<%= bootstrap %>/dist/fonts', src: ['*.*'], dest: '<%= site.assets %>/fonts/' },
+          { expand: true, cwd: '<%= bootstrap %>/dist/js', src: ['*.*'], dest: '<%= site.assets %>/js/' },
         ]
-      }
-    },
+            }
+        },
 
-    watch: {
-      all: {
-        files: ['<%= jshint.all %>'],
-        tasks: ['jshint', 'nodeunit']
-      },
-      site: {
-        files: ['Gruntfile.js', '<%= less.options.paths %>/*.less', 'templates/**/*.hbs'],
-        tasks: ['design']
-      }
-    }
-  });
+        watch: {
+            all: {
+                files: ['<%= jshint.all %>'],
+                tasks: ['jshint', 'nodeunit']
+            },
+            site: {
+                files: ['Gruntfile.js', '<%= less.options.paths %>/*.less', 'templates/**/*.hbs'],
+                tasks: ['design']
+            }
+        },
 
-  // Load npm plugins to provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-readme');
-  grunt.loadNpmTasks('grunt-sync-pkg');
-  grunt.loadNpmTasks('assemble-less');
-  grunt.loadNpmTasks('assemble');
+        gitcheckout: {
+            src: {
+                options: {
+                    branch: 'src'
+                }
+            },
+            master: {
+                options: {
+                    branch: 'master'
+                }
+            }
+        }
+    });
 
-  // Build HTML, compile LESS and watch for changes. You must first run "bower install"
-  // or install Bootstrap to the "vendor" directory before running this command.
-  grunt.registerTask('design', ['clean', 'assemble', 'less:site', 'watch:site']);
+    // Load npm plugins to provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-readme');
+    grunt.loadNpmTasks('grunt-sync-pkg');
+    grunt.loadNpmTasks('grunt-git');
+    grunt.loadNpmTasks('assemble-less');
+    grunt.loadNpmTasks('assemble');
 
-  grunt.registerTask('docs', ['readme', 'sync']);
+    // Build HTML, compile LESS and watch for changes. You must first run "bower install"
+    // or install Bootstrap to the "vendor" directory before running this command.
+    grunt.registerTask('design', ['clean', 'assemble', 'less:site', 'watch:site']);
 
-  grunt.registerTask('default', ['clean', 'jshint', 'copy:assets', 'assemble', 'less', 'docs']);
+    grunt.registerTask('docs', ['readme', 'sync']);
+
+    grunt.registerTask('default', ['clean', 'jshint', 'copy:assets', 'assemble', 'less', 'docs']);
+
+    grunt.registerTask('deploy', ['gitcheckout:master', 'gitcheckout:src']);
 };
